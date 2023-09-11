9279d2cb15acdcbf394c13ff4e8728664d31f055
Phoebe Chuang <phoebe160723@gmail.com>
2023-09-11 12:39:27 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -12,7 +12,7 @@ class FizzBuzz {
         }
     }
 
-    private static int checkingLoop(int i) {
+    public static int checkingLoop(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
