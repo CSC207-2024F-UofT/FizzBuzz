4bbf4dd9f9e7a9df35d89026cf2f7f1cc6cd0d40
ShihHsin0723 <144292774+ShihHsin0723@users.noreply.github.com>
2023-09-12 00:43:51 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        FizzBuzz.checkingLoop(5);
+        FizzBuzz.checkingLoop(5
+        System.out.println("The change");
     }
 }
