846ce969b0cc11002887d9f89dcec4b2eba5ff46
becca776 <rrjkong@gmail.com>
2023-09-11 16:30:33 -0400

    made doFizzBuzz public

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -16,7 +16,7 @@ class FizzBuzz {
         }
     }
 
-    private static void doFizzBuzz(int i) {
+    public static void doFizzBuzz(int i) {
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
 
