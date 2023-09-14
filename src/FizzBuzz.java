3170d2532a9665030e2659189baad9cb6c2430a9
Zhang <zhenyi.zhang@mail.utoronto.ca>
2023-09-13 21:01:15 -0400

    hhh

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,7 +5,7 @@ class FizzBuzz {
 
     public static void main(String[] args) {
         for (int i = 1; i < 100; i++) {
-            // fgdeg
+            // fgdegg
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
             boolean divisibleBy5 = i % 5 == 0;
