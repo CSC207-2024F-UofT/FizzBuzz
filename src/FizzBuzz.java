9fef57ed20b531174a0ffa158a4422e4b2810ce6
Christopher Deng <chrisdeng@gmail.com>
2024-09-09 12:27:28 -0400

    Made doFizzBuzz public

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -12,7 +12,7 @@ class FizzBuzz {
         }
     }
 
-    private static int doFizzBuzz(int i) {
+    public static int doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
