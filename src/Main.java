ecf2f485704268a78228f049256a3903c3329b19
JonDD-D <dengjohn123@163.com>
2024-09-09 20:13:52 -0400

    change for main.java inside src folder blah

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        fizzBuzz.main(args);
         int i = 100;
         while (i<123){
             i = fizzBuzz.doFizzBuzz(i);
