edc2978ad4efbef0df58fe8ee2463bb4e263de3d
hinobino <hinar.alsharkawy@mail.utoronto.ca>
2024-09-23 00:52:57 -0400

    add code to class reduce

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int c = 0;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+                c++;
+            } if (n % 2 != 0) {
+                n--;
+                c++;
+            }
+        } if (n == 0) {
+            System.out.println(c);
+        }
+    }
 }
